6ab386f6b7c6c1bf184449b0346085bb5d72087d
Alptekin Gürsoy <42710814+agursoyy@users.noreply.github.com>
2020-03-20 01:06:39 +0300

    Update stringify.ts

--- app/utils/stringify.ts
+++ app/utils/stringify.ts
@@ -1,19 +1,4 @@
-import { toJS } from 'mobx';
+import Stringfy from 'json-stringify-safe';
+const storeData = (data: object) => Stringfy(data, null, 0);
 
-const Stringify = (data: object): object =>
-  Object.entries(data).reduce(
-    (prev, [key, value]) =>
-      value && typeof value === 'object'
-        ? {
-          ...prev,
-          ...(Array.isArray(value)
-            ? { [key]: value }
-            : value.constructor && value.constructor.name === 'Store'
-              ? {}
-              : { [key]: Stringify(toJS(value)) }),
-        }
-        : { ...prev, [key]: value },
-    {},
-  );
-
-export default Stringify;
+export default storeData;
